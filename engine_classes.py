2f0077f1d000446f5baffa699c3df6ad444de739
marie-george <maria85night@gmail.com>
2023-03-29 01:17:00 +0300

    Создание класса HeadHunter, наследуемого от класса Engine

--- engine_classes.py
+++ engine_classes.py
@@ -66,4 +66,43 @@ class SuperJob(Engine):
         return vacancies
 
 
+class HeadHunter(Engine):
+    """Класс для парсинга вакансий с сайта HeadHunter"""
+
+    @staticmethod
+    def _get_salary(salary_info: dict):
+        """Обработка поля salary(зарплата): предпочтительно выводить зарплату 'от', если же она не указана,
+                то выводить зарплату 'до'. Или выводить 0, если поле отсутствует"""
+        if salary_info:
+            if salary_info.get('to'):
+                return salary_info['to']
+            if salary_info.get('from'):
+                return salary_info['from']
+        return 0
+
+    @staticmethod
+    def _get_remote_work(remote_work_info: dict):
+        """Обработка поля remote_work(удаленная работа)"""
+        if remote_work_info:
+            if remote_work_info['id'] == 'fullDay':
+                return 'В офисе'
+            if remote_work_info['id'] == 'remote':
+                return 'Удаленно'
+        return 'Другое'
+
+    def get_request(self, keyword):
+        """Парсинг 500 вакансий и создание из них объекта типа list"""
+        vacancies = []
+        for page_no in range(5):
+            response = requests.get(f"https://api.hh.ru/vacancies?text={keyword}", params={'per_page': '100', 'page': page_no}).json()
+            for vacancy in response['items']:
+                vacancies.append({
+                    "name": vacancy['name'],
+                    "company_name": vacancy['employer']['name'],
+                    "url": vacancy['alternate_url'],
+                    "description": vacancy['snippet']['requirement'],
+                    "remote_work": self._get_remote_work(vacancy.get('schedule', {})),
+                    "salary": self._get_salary(vacancy.get('salary', {})),
+                })
+        return vacancies
 
