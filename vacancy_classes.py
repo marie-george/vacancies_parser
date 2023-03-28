e590d16af2f8c9c820c8040cc34da789bd331cb6
marie-george <maria85night@gmail.com>
2023-03-29 01:24:28 +0300

    Создание класса CountMixin, который возвращает количество вакансий из файла с вакансиям

--- vacancy_classes.py
+++ vacancy_classes.py
@@ -23,3 +23,18 @@ class Vacancy:
         return self.salary > other.salary
 
 
+class CountMixin:
+    """Возвращает количество вакансий из файла с вакансиями"""
+
+    def __init__(self, file_name=None):
+        self.file_name = file_name
+
+    def get_count_of_vacancy(self):
+
+        with open(self.file_name, "r", encoding="UTF-8") as file:
+            data = json.load(file)
+            counter = 0
+            for i in data:
+                counter += 1
+        return counter
+
